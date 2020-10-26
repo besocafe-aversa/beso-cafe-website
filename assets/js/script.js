fb2b8616d473cf35f3e03d2638a794b45df833fc
Luigi Barbato <l.barbato11@studenti.unisa.it>
2020-10-26 11:42:43 +0100

    Content Updated.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,39 @@
     $(this).parent().find('.ti-angle-down').removeClass('ti-angle-down').addClass('ti-angle-right');
   });
 
-  
-	//slider
-	$('.slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		dots: true,
-		arrows: false
-	});
+ // slick slider
+ $('.gallery-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: true,
+  prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'fa fa-angle-left\'></i></button>',
+  nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'fa fa-angle-right\'></i></button>',
+  autoplay: true,
+  responsive: [{
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+          }
+      },
+      {
+          breakpoint: 401,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      }
+  ]
+});
 
 })(jQuery);
--- exampleSite/content/menu/_index.md
+++
@@ -1,6 +0,0 @@
----
-title: "Platform"
-subtitle: "How our platform works"
-# meta description
-description: "How our platform works guide"
----
